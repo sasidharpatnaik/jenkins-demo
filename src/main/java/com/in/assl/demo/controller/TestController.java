381afae76fc9e31a05ca379ce7103724c189cfe3
Sasidhar Kuppili <sasidhar.kuppili@gmail.com>
2024-03-18 21:28:18 +0530

    initial commit

---
+++ src/main/java/com/in/assl/demo/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.in.assl.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+    @GetMapping("/welcome/{name}")
+    public ResponseEntity<String> sayHello(@PathVariable String name) {
+        return new ResponseEntity<>("Hello "+name, HttpStatus.OK);
+    }
+}
